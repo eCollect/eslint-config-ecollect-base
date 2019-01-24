71f375e2488ecaa0d77944bfe611e0310fffccd9
Dimitar Nanov <dimitar.nanov@ecollect.net>
2019-01-24 07:32:35 +0200

    feat: remove airbnb and imort dependency
    * rule packages re migrated from airbnbn-base and adjusted
    * node irrelevant sections are removed ( mainly import )

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 This package provides eCollect's base JS .eslintrc as an extensible shared config.
 
-It is heavily based on [Airbnb's base package](https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb-base) with some rules extended and changed to match our eCollect's own guides.
+It is heavily based on [Airbnb's base package](https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb-base) with some rules extended and changed to match eCollect's own guides.
+The base package is aimed at node.js development.
 
 ## Usage
 
-Our default export contains all of our ESLint rules, including ECMAScript 6+. It requires `eslint` and `eslint-plugin-import`.
+Our default export contains all of our ESLint rules, including ECMAScript 6+. It requires `eslint`.
 
 1. Install the correct versions of each package, which are listed by the command:
 
--- index.js
+++ index.js
@@ -1,48 +1,19 @@
+'use strict';
+
 module.exports = {
-	extends: 'airbnb-base',
+	extends: [
+		'./rules/best-practices',
+		'./rules/errors',
+		'./rules/node',
+		'./rules/style',
+		'./rules/variables',
+		'./rules/es6',
+	].map(require.resolve),
 	env: {
-		node: true
+		node: true,
 	},
 	parserOptions: {
 		ecmaVersion: 2018,
 		sourceType: 'script',
-		ecmaFeatures: {
-			modules: false,
-		}
 	},
-	rules: {
-		'max-len': 0,
-		'linebreak-style': 0,
-		'no-plusplus': [
-			2,
-			{
-				allowForLoopAfterthoughts: true
-			}
-		],
-		'no-continue': 0,
-		indent: [
-			2,
-			'tab'
-		],
-		'nonblock-statement-body-position': 'off',
-		'no-mixed-operators': [ 'error', { allowSamePrecedence: true }],
-		'no-tabs': 0,
-		strict: [
-			2,
-			'safe'
-		],
-		curly: [
-			2,
-			'multi',
-			'consistent'
-		],
-		'no-restricted-syntax': [ 'error', 'ForInStatement', 'LabeledStatement', 'WithStatement' ],
-		'no-await-in-loop': 'off',
-		'import/no-extraneous-dependencies': 0,
-		'import/no-unresolved': 0,
-		'no-underscore-dangle': 0,
-		'no-param-reassign': 0,
-		'generator-star-spacing': 0,
-		'jsx-a11y/href-no-hash': 'off'
-	}
 };
